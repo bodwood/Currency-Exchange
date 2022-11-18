02ea88a11c166e44f8e32f8c75fdcefbe45d6a96
bodwood <bodiewood@hotmail.com>
2022-11-18 11:41:44 -0800

    add getCurrency class to currencyExchange.js. This class will call the API and handle invalid API responses

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/styles.css'
 
+
+// async function getCurrency(){
+//   const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+//   const jsonResponse = await response.json();
+//   return jsonResponse;
+// }
---
+++ src/js/currencyExchange.js
@@ -0,0 +1,15 @@
+export default class CurrencyExchange {
+  static async getCurrency(currency) {
+    try{
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
+      const jasonResponse = await response.json();
+      if(!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jasonResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jasonResponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
